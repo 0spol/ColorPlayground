0f9b6c5af54699c14bd19a0dc2dd61950df710af
Kamilly-cod <kamillybritodm@gmail.com>
2025-02-23 19:20:49 +0100

    Fenix: Commits que había eliminado antes, sin querer.

--- app/src/main/java/com/colorplayground/application/ui/theme/Color.kt
+++ app/src/main/java/com/colorplayground/application/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Colores del proyecto
+val black_Color = Color(0xFF000000)
+val white_Color = Color(0xFFFFFFFF)
+val new_tint_color = Color(0xFF262626)
+val default_tint_color = Color(0xFF595959)
+
+// Nuevas colores
+val verde_neon = Color(0xFF0CF25D)
+val verde_medio = Color(0xFF038C3E)
+val verde_medio_escoro = Color(0xFF02735E)
+val verde_escuro = Color(0xFF025951)
